ea663e1473b273c28d1610e18f95a8a7248df5b4
Ricardo Yang <ricardo.yang00@gmail.com>
2024-02-29 16:22:32 +0000

    tation.h

---
+++ include/station.h
@@ -0,0 +1,15 @@
+#ifndef PROJ_DA_01_STATION_H
+#define PROJ_DA_01_STATION_H
+
+#include <string>
+
+class Station {
+public:
+    Station();
+    Station(int id, std::string code);
+private:
+    int id;
+    std::string code;
+};
+
+#endif //PROJ_DA_01_STATION_H
