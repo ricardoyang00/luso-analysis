a68f301c80b1a52db979d18ce4ae398862880cc9
Ricardo Yang <ricardo.yang00@gmail.com>
2024-02-29 16:18:37 +0000

    reservoir.h

---
+++ reservoir.h
@@ -0,0 +1,24 @@
+#ifndef PROJ_DA_01_RESERVOIR_H
+#define PROJ_DA_01_RESERVOIR_H
+
+#include <string>
+
+class Reservoir {
+public:
+    Reservoir();
+    Reservoir(std::string name, std::string municipality, int id, std::string code, int maxDelivery);
+    std::string getName();
+    std::string getMunicipality();
+    int getId();
+    std::string getCode();
+    int getMaxDelivery();
+
+private:
+    std::string name;
+    std::string municipality;
+    int id;
+    std::string code;
+    int maxDelivery;
+};
+
+#endif //PROJ_DA_01_RESERVOIR_H
