bcce8929f2ad875919236bcb06f1d9b13adf46a1
Ricardo Yang <ricardo.yang00@gmail.com>
2024-03-01 11:47:40 +0000

    implement public functions of Pipe class

--- include/Pipe.h
+++ include/Pipe.h
@@ -6,12 +6,11 @@
 
 class Pipe {
 public:
-    Pipe();
     Pipe(Code sourceCode, Code targetCode, int capacity, bool direction);
-    Code getSourceCode();
-    Code getTargetCode();
-    int getCapacity();
-    bool getDirection();
+    Code getSourceCode() const;
+    Code getTargetCode() const;
+    int getCapacity() const;
+    bool getDirection() const;
 
 private:
     Code sourceCode;
@@ -19,5 +18,24 @@ private:
     int capacity;
     bool direction;         //undirected - 1 , bidirectional - 0
 };
+
+Pipe::Pipe(Code sourceCode, Code targetCode, int capacity, bool direction)
+        : sourceCode(sourceCode), targetCode(targetCode), capacity(capacity), direction(direction) {}
+
+Code Pipe::getSourceCode() const {
+    return sourceCode;
+}
+
+Code Pipe::getTargetCode() const {
+    return targetCode;
+}
+
+int Pipe::getCapacity() const {
+    return capacity;
+}
+
+bool Pipe::getDirection() const {
+    return direction;
+}
 
 #endif //PROJ_DA_01_PIPE_H
