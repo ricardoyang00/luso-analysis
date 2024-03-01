194b4d493aeafc811f768d42d1dbbaeb544b1107
Ricardo Yang <ricardo.yang00@gmail.com>
2024-03-01 11:54:07 +0000

    implement public functions of City class

--- include/City.h
+++ include/City.h
@@ -6,13 +6,12 @@
 
 class City {
 public:
-    City();
     City(std::string name, int id, Code code, int demand, int population);
-    std::string getName();
-    int getId();
-    Code getCode();
-    int getDemand();
-    int getPopulation();
+    const std::string getName() const;
+    int getId() const;
+    Code getCode() const;
+    int getDemand() const;
+    int getPopulation() const;
 private:
     std::string name;
     int id;
@@ -20,5 +19,28 @@ private:
     int demand;
     int population;
 };
+
+City::City(std::string name, int id, Code code, int demand, int population)
+        : name(name), id(id), code(code), demand(demand), population(population) {}
+
+const std::string City::getName() const {
+    return name;
+}
+
+int City::getId() const {
+    return id;
+}
+
+Code City::getCode() const {
+    return code;
+}
+
+int City::getDemand() const {
+    return demand;
+}
+
+int City::getPopulation() const {
+    return population;
+}
 
 #endif //PROJ_DA_01_CITY_H
