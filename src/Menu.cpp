74e3a7e4901921c1a977f244dd1b30deca777d92
Bruno Huang <brunohuang2012@gmail.com>
2024-04-06 11:53:26 +0100

    added a notification to reset graph after balance

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -86,6 +86,7 @@ int Menu::run() {
             case 5:
                 bsm.balanceFlow();
                 cout << "Balance algorithm successfully terminated, please check [4] again to see the differences" << endl;
+                cout << makeBold("⚠\uFE0FAFTER THIS PLEASE ENSURE THAT YOU RESET THE GRAPH TO AS IT WAS BY CLICKIG [2] Total Maximum Flow⚠\uFE0F") << endl;
                 break;
             case 6:     // remove reservoir
                 clearScreen();
